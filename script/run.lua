bd26fe108eb572a061e9e6b8d6175462fd9b909c
lisasmirk <lisa.smirk15@gmail.com>
2025-07-24 14:10:17 +0200

    Create run.lua

---
+++ script/run.lua
@@ -0,0 +1,3 @@
+MinimumProfitPercentage = 6 -- for auto offer calculations!
+AutoTrade = false 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/X/refs/heads/main/mm2-trade-helper.lua",true))()
